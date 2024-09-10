1480ae22934381f31fec20ee530f0e6ab677b285
YoCere <jcerezorios@gmail.com>
2024-09-10 14:41:23 -0400

    Actualizando vite para que funcione en el css

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-    server: {
-        https: true, // Agrega esta línea para forzar HTTPS
-        hmr: {
-            host: 'inventario-laravel-production.up.railway.app', // Agrega tu dominio aquí
-            protocol: 'wss', // Cambia a 'wss' (WebSocket Secure) para HTTPS
-        },
-    },
+export default defineConfig(({ command, mode }) => {
+    return {
+        plugins: [
+            laravel({
+                input: [
+                    'resources/css/app.css',
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+        ],
+        // Solo habilitar el servidor HTTPS y HMR si estás en modo de desarrollo
+        server: command === 'serve' && mode === 'development' ? {
+            https: true, // Forzar HTTPS solo en desarrollo
+            hmr: {
+                host: 'inventario-laravel-production.up.railway.app', // Tu dominio para HMR en desarrollo
+                protocol: 'wss', // Usar WebSocket seguro (wss)
+            },
+        } : {},
+    }
 });
