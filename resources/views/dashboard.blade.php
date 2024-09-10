09087487f2f3e1d1dbb3f1c39dbcd1f3d8bd97c4
YoCere <jcerezorios@gmail.com>
2024-09-10 00:04:07 -0400

    configuracion de vite para hosting railway

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight" bg>
             {{ __('Pagina principal') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-1 bg-gray-900">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-gray-100 overflow-hidden text-gray-100 shadow-xl sm:rounded-lg">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos aspernatur dicta optio aliquid eaque necessitatibus facilis culpa maiores corrupti explicabo veniam assumenda illo quidem placeat fuga molestias sapiente, voluptatibus consequuntur.</p>
                 <p>Alias rerum provident blanditiis facere! Est qui, dolores repudiandae quam ratione ut. Hic at mollitia quidem, sapiente magnam vel molestias fugit sed voluptatum quas harum, nemo debitis odio fugiat iure.</p>
                 <p>Tempore nostrum eveniet provident dolor exercitationem eaque sit. Distinctio officiis deleniti similique dignissimos qui, commodi vitae recusandae error aliquid, at aperiam atque corporis quas veritatis maxime, quaerat eaque hic nulla?</p>
--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        https: true, // Agrega esta línea para forzar HTTPS
+        hmr: {
+            host: 'inventario-laravel-production.up.railway.app', // Agrega tu dominio aquí
+            protocol: 'wss', // Cambia a 'wss' (WebSocket Secure) para HTTPS
+        },
+    },
 });
